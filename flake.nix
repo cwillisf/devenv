270103ab29236f9168a75ea7ad9f5b0c299a6209
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2025-03-03 19:49:06 -0800

    fix: add bashInteractive to fix subshells

--- flake.nix
+++ flake.nix
@@ -52,6 +52,7 @@
             modules = [
               {
                 packages = [
+                  pkgs.bashInteractive
                   pkgs2.chromedriver
                   pkgs.git
                   pkgs2.vscode.fhs
