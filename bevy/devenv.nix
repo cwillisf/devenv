6f795a8dce1f01dd0a348e8e117f0f265c7e1ff5
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2025-04-10 22:31:29 -0700

    add treefmt-nix

--- bevy/devenv.nix
+++ bevy/devenv.nix
@@ -1,2 +1,13 @@
-_: {
+{ pkgs, inputs, ... }:
+let
+  treefmt-nix = (import inputs.treefmt-nix).mkWrapper pkgs {
+    programs.rustfmt.enable = true;
+  };
+in {
+  pre-commit.hooks = {
+    treefmt = {
+      enable = true;
+      package = treefmt-nix;
+    };
+  };
 }
