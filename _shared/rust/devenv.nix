dcada1bce43b6fdc02f26f88150bf9fe9eb53033
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2025-04-12 15:19:56 -0700

    switch rust to stable channel, add rust-src

--- _shared/rust/devenv.nix
+++ _shared/rust/devenv.nix
@@ -1,5 +1,19 @@
 _: {
-  languages.rust.enable = true;
+  languages.rust = {
+    enable = true;
+    channel = "stable";
+    components = [
+      # Defaults
+      "rustc"
+      "cargo"
+      "clippy"
+      "rustfmt"
+      "rust-analyzer"
+
+      # Extras
+      "rust-src"
+    ];
+  };
 
   pre-commit.hooks = {
     cargo-check.enable = true;
