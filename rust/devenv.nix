bca739d84c35efcd89e88d35c4eeb421e06f4f18
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2025-04-11 22:31:28 -0700

    add devenv for rust

--- rust/devenv.nix
+++ rust/devenv.nix
@@ -3,13 +3,17 @@ let
   treefmt-nix = (import inputs.treefmt-nix).mkWrapper pkgs;
 in
 {
-  # languages.rust.enable = true;
+  languages.rust.enable = true;
 
   pre-commit.hooks = {
+    cargo-check.enable = true;
+    check-toml.enable = true;
+    clippy.enable = true;
     treefmt = {
       enable = true;
       package = treefmt-nix {
-        # programs.rustfmt.enable = true;
+        programs.rustfmt.enable = true;
+        programs.taplo.enable = true;
       };
     };
   };
