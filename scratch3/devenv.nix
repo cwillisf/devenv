c617a3d5a9ba46b4a3d40a585fa09fad36e90ced
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2025-04-14 13:15:44 -0700

    first attempt at a Scratch 3 devenv in this style

---
+++ scratch3/devenv.nix
@@ -0,0 +1,30 @@
+{ pkgs, inputs, ... }:
+let
+  treefmt-nix = (import inputs.treefmt-nix).mkWrapper pkgs;
+in
+{
+  languages.javascript.package = pkgs.nodejs_20; # not slim = includes npm
+
+  pre-commit.hooks = {
+    treefmt = {
+      enable = true;
+      package = treefmt-nix {
+        programs = {
+          actionlint.enable = true;
+          yamlfmt = {
+            enable = true;
+            settings = {
+              formatter = {
+                retain_line_breaks_single = true;
+              };
+            };
+          };
+        };
+      };
+    };
+    yamllint = {
+      enable = true;
+      settings.configData = "{extends: default, rules: {document-start: disable}}";
+    };
+  };
+}
