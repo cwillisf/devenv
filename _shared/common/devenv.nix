68ea7aa0a04645e7a0cb432f64af66f04f8cdb47
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2025-04-14 10:54:23 -0700

    enable more pre-commit hooks

--- _shared/common/devenv.nix
+++ _shared/common/devenv.nix
@@ -24,6 +24,8 @@ in
     "pre-commit-hooks"
   ];
 
+  languages.shell.enable = true;
+
   packages = [
     pre-commit-install-devenv-hooks
     pkgs.bashInteractive
@@ -35,9 +37,25 @@ in
   pre-commit.hooks = {
     check-added-large-files.enable = true;
     check-case-conflicts.enable = true;
+    check-executables-have-shebangs.enable = true;
+    check-json.enable = true;
     check-merge-conflicts.enable = true;
     check-symlinks.enable = true;
+    detect-aws-credentials.enable = true;
+    detect-private-keys.enable = true;
     editorconfig-checker.enable = true;
+    end-of-file-fixer.enable = true;
+    fix-byte-order-marker.enable = true;
+    gitlint = {
+      enable = true;
+      # this hack is sad for at least two reasons:
+      # - it'd be unnecessary if `gitlint.args` were inserted before `--msg-filename`
+      # - it uses `gitlint` from `$PATH` which might not be the one installed by this hook
+      #   (it could be an incompatible version or even some unrelated program of the same name)
+      entry = "gitlint --staged --ignore body-is-missing --msg-filename";
+    };
+    html-tidy.enable = true;
+    lychee.enable = true;
     markdownlint = {
       enable = true;
       settings.configuration = {
@@ -46,7 +64,10 @@ in
         };
       };
     };
+    mixed-line-endings.enable = true;
     ripsecrets.enable = true;
+    shellcheck.enable = true;
+    trim-trailing-whitespace.enable = true;
     trufflehog.enable = true;
     typos = {
       enable = true;
--- nix/devenv.nix
+++ nix/devenv.nix
@@ -6,6 +6,7 @@ in
   languages.nix.enable = true;
   pre-commit.hooks = {
     nil.enable = true;
+    pre-commit-hook-ensure-sops.enable = true; # pre-commit hook to ensure that files that should be encrypted with sops are
     statix.enable = true;
     treefmt = {
       enable = true;
@@ -14,8 +15,20 @@ in
           deadnix.enable = true;
           nixfmt.enable = true;
           statix.enable = true;
+          yamlfmt = {
+            enable = true;
+            settings = {
+              formatter = {
+                retain_line_breaks_single = true;
+              };
+            };
+          };
         };
       };
+    };
+    yamllint = {
+      enable = true;
+      settings.configData = "{extends: default, rules: {document-start: disable}}";
     };
   };
 }
