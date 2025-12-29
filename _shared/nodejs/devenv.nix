ca6af78cafd7d00bfee08783d0a23e1f09d2f592
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2025-12-29 13:44:48 -0800

    fix Node.js version override

--- _shared/nodejs/devenv.nix
+++ _shared/nodejs/devenv.nix
@@ -14,9 +14,17 @@
   config = {
     languages.javascript = {
       enable = true;
-      package = lib.mkIf (
-        config.devenv-override.node-major != ""
-      ) pkgs."nodejs_${config.devenv-override.node-major}";
+      # Based on the documentation, I'd expect that
+      # - setting `npm.enable` to true and
+      # - using the `-slim` package
+      # would result in installing the latest `npm` even for older versions of Node.
+      # Instead, the result seems identical to setting `npm.enable` to false and using the non-slim package.
+      npm.enable = true;
+      package =
+        lib.mkDefault
+          pkgs."nodejs-slim${
+            lib.optionalString (config.devenv-override.node-major != "") "_${config.devenv-override.node-major}"
+          }";
     };
 
     git-hooks.hooks = {
