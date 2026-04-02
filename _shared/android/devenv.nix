4577305da5060ddebe9ecf348803b70fef396929
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2026-04-02 10:28:14 -0700

    put .android directory under .devenv

--- _shared/android/devenv.nix
+++ _shared/android/devenv.nix
@@ -1,4 +1,5 @@
-_: {
+{ config, ... }:
+{
   android = {
     enable = true;
     abis = [ "x86_64" ];
@@ -6,4 +7,14 @@ _: {
     systemImages.enable = true;
     android-studio.enable = true;
   };
+
+  # Override devenv's default of $(pwd)/.android so that Android user data
+  # (AVDs, caches) doesn't pollute the project directory.
+  # Using enterShell because the android module sets ANDROID_USER_HOME
+  # in a shell hook that runs after env vars are exported.
+  enterShell = ''
+    export ANDROID_USER_HOME="${config.devenv.dotfile}/.android"
+    export ANDROID_AVD_HOME="$ANDROID_USER_HOME/avd"
+    mkdir -p "$ANDROID_USER_HOME" "$ANDROID_AVD_HOME"
+  '';
 }
